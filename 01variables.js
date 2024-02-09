c8bc316b35930e64a91f091034ed51a58e122e10
dheerajjha3123 <dheerajjha11111@gmail.com>
2024-02-09 20:24:18 +0530

    Create 01variables.js

---
+++ 01variables.js
@@ -0,0 +1,15 @@
+const accountID=84544 // const value can not be change it's known as predefined value
+let accountEmail="dheeraj@gmail.com"
+var accountPassword="123456"
+accountCity="Delhi"
+let accountState // if we don't declare any value js automatically detect and undefined value
+
+// accountID=2  // cannot change
+
+
+accountEmail="newdhee@gmail.com"
+accountPassword="5251151"
+accountCity="Noida"
+
+console.log(accountID);
+console.table([accountEmail,accountPassword,accountCity,accountState]) // show all data in table
