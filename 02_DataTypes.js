8ff0dcd223d760f9f4805752bf2fce9a4cfabf28
dheerajjha3123 <dheerajjha11111@gmail.com>
2024-02-12 11:05:32 +0530

    Data types

---
+++ 02_DataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // Treat all JS code as Newer Version
+
+// alert(3+3) // We are using nodejs, not browser that's why showing error
+
+console.log(3 +3)
+
+console.log("Hello")
+
+let name="Sakuna" // This is String type Data
+let age=20 // Number type data
+let isLoggin= true // boolean
+
+ // number => 2 to power 53
+ // bigint
+ // string =>""
+ // boolean = true/ false
+ // null => standlone value
+ // undefined =>
+ // symbol => unique
+
+ //object
+
+ console.log(typeof "Sakuna"); // showing type
+ console.log(typeof null); // object
+ console.log(typeof undefined); // undefined
---
+++ 03_conversion&Operation.js
@@ -0,0 +1,24 @@
+let score ="89adbg"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let ValueNumber= Number(score)
+
+//console.log(typeof ValueNumber);
+//console.log(ValueNumber);
+
+// "89" => 89 convert to number
+//"89adbg" => NaN
+
+let isLoggedin =1 
+// 1 = true ; 0 = false
+// "" = false ; "example" = true
+let checkLogin=Boolean(isLoggedin)
+
+//console.log(checkLogin);
+
+let checkString =89
+let convertString = String(checkString)
+console.log(convertString);
+console.log(typeof convertString);
